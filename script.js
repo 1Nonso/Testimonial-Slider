7ce9abaa914534e4949af0a538fe14eb8ef4a9b3
Othniel <chukchi2345@gmail.com>
2024-12-18 16:15:19 +0100

    Made modifications in index.html and script,js, mainly focused on the web-app functionality

--- script.js
+++ script.js
@@ -3,32 +3,22 @@ const buttonPrev = document.querySelector(".imageContainer");
 const buttonNext = document.querySelector(".imageContainer2");
 const button = document.querySelector(".btn");
 const text = document.querySelector(".comment");
+const devName = document.querySelector(".devName");
+const profession = document.querySelector(".profession");
 
-// buttonPrev.addEventListener("click", () => {
-//   //   text.style.backgroundColor = "red";
-//   //   console.log(buttonPrev);
-//   if (image.src.match("assets/images/image-tanya.jpg")) {
-//     image.src = "assets/images/image-john.jpg";
-//   } else {
-//     image.src = "assets/images/image-tanya.jpg";
-//   }
-// });
+const tanyaMessage =
+  "“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”";
 
-// buttonNext.addEventListener("click", () => {
-//   if (image.src.match("assets/images/image-tanya.jpg")) {
-//     image.src = "assets/images/image-john.jpg";
-//   } else {
-//     image.src = "assets/images/image-tanya.jpg";
-//   }
-// });
+const johnMessage =
+  " “ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”";
 
-const changeComment = [
-  {
-    comment: "heyy",
-    userName: "John Tarkpor",
-    profession: "Junior Front-end Developer",
-  },
-];
+const tanya = "Tanya Sinclair";
+
+const john = "John Tarkpor";
+
+const tanyaProfession = "UX Engineer";
+
+const johnProfession = "Front-end Developer";
 
 button.addEventListener("click", () => {
   if (image.src.match("assets/images/image-tanya.jpg")) {
@@ -36,11 +26,19 @@ button.addEventListener("click", () => {
   } else {
     image.src = "assets/images/image-tanya.jpg";
   }
-  if (text.textContent.includes("been")) {
-    text.textContent =
-      " “ If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ” ";
+  if (text.textContent.match(tanyaMessage)) {
+    text.textContent = johnMessage;
   } else {
-    text.textContent =
-      " “ I've been interested in coding for a while but never taken the jump, until now. I couldn't recommend this course enough. I'm now in the job of my dreams and so excited about the future. ” ";
+    text.textContent = tanyaMessage;
+  }
+  if (devName.textContent.match(tanya)) {
+    devName.textContent = john;
+  } else {
+    devName.textContent = tanya;
+  }
+  if (profession.textContent.match(tanyaProfession)) {
+    profession.textContent = johnProfession;
+  } else {
+    profession.textContent = tanyaProfession;
   }
 });
