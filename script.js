77d8298b229a873efa68d7cbdc44a10f01cdf5e4
Othniel <chukchi2345@gmail.com>
2024-12-21 22:21:24 +0100

    I just finalized the phase 1 of the project, corrected some functionality in the JS file

--- script.js
+++ script.js
@@ -32,44 +32,21 @@ button.addEventListener("click", () => {
 
   image.src = currentProfile.image;
   text.textContent = currentProfile.message;
-  devName.textContent = currentProfile.name;
-  profession.textContent = currentProfile.profession;
+
+  // devName.textContent = currentProfile.name;
+  // Create temporary div elements
+  const nameElement = document.createElement("strong");
+  nameElement.textContent = currentProfile.name;
+  devName.innerHTML = ""; // Clear existing content
+  devName.appendChild(nameElement);
+
+  const profElement = document.createElement("span");
+  profElement.style.opacity = "0.5";
+  profElement.textContent = currentProfile.profession;
+  profession.innerHTML = ""; // Clear existing content
+  profession.appendChild(profElement);
+
+  // profession.textContent = currentProfile.profession;
 });
 
-// const tanyaMessage =
-//   "“I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.”";
-
-// const johnMessage =
-//   "“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.”";
-
-// const tanya = "Tanya Sinclair";
-
-// const john = "John Tarkpor";
-
-// const tanyaProfession = "UX Engineer";
-
-// const johnProfession = "Front-end Developer";
-
-// button.addEventListener("click", () => {
-
-//   if (image.src.match("assets/images/image-tanya.jpg")) {
-//     image.src = "assets/images/image-john.jpg";
-//   } else {
-//     image.src = "assets/images/image-tanya.jpg";
-//   }
-//   if (text.textContent === tanyaMessage) {
-//     text.textContent = johnMessage;
-//   } else {
-//     text.textContent = tanyaMessage;
-//   }
-//   if (devName.textContent.match(tanya)) {
-//     devName.textContent = john;
-//   } else {
-//     devName.textContent = tanya;
-//   }
-//   if (profession.textContent.match(tanyaProfession)) {
-//     profession.textContent = johnProfession;
-//   } else {
-//     profession.textContent = tanyaProfession;
-//   }
-// });
+// For profession with opacity
